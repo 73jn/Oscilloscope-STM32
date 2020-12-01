01e3c9e63c9cfbd41e7f3e113356168a9a4127ab
73jn <jean.nanchen@gmail.com>
2020-12-01 14:41:31 +0100

    Add adc and isrs.cpp

---
+++ SWSTM32-Oscilloscope-521/Src/isrs.cpp
@@ -0,0 +1,14 @@
+/*
+ * isrs.cpp
+ *
+ *  Created on: 1 déc. 2020
+ *      Author: jeann
+ */
+
+#include "stm32f7xx_hal.h"
+
+
+extern "C" void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* AdcHandle)
+{
+
+}
